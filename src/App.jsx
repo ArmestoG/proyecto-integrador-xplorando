ee922c34379106acf6254f0f5535c5b47878f4f2
Felipe Burbano <pipeflute9@gmail.com>
2024-02-26 01:50:42 -0500

    Subí el body

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom'; // Importa BrowserRouter si estás utilizando React Router
 import Header from './components/Header/Header'; 
+import Body  from './components/Body/Body';
 import Footer from './components/Footer/Footer';
 import AgregarProducto from './components/FormAgregarProducto/AgregarProducto';
+
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Body/>
         <AgregarProducto/>
         <Footer />
       </div>
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Body.css'; // Estilos CSS
+
+
+const TuComponenteReact = () => {
+  return (
+    <div>
+      {/* Barra de Búsqueda */}
+      <div className="container">
+        <input type="text" placeholder="Buscar" />
+        <div className="btn">
+          <i className="Barra busqueda"></i>
+        </div>
+      </div>
+
+      {/* Responsive Cards */}
+      <div className="slide-container">
+        <div className="slide-content">
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="image-content">
+                <span className="overlay"></span>
+
+                <div className="card-image">
+                  <img 
+                  src="./src/assets/cards/esquiando.png" alt="" className="card-img" />
+                </div>
+              </div>
+              <div className="card-content">
+                <h2 className="name">Turismo aventura</h2>
+                <h3 className="price">$1.000.000 precio del paquete</h3>
+                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                <button className="button">Ver más información</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TuComponenteReact;
