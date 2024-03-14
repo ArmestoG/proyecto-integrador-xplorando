fda4281ce2cc6b16b00c74eb88c60b0881909d2e
ArmestoG <ger.armesto@gmail.com>
2024-03-14 03:05:50 -0300

    panel admin casi que terminado

--- src/components/ListaProductos/ListaProductos.jsx
+++ src/components/ListaProductos/ListaProductos.jsx
@@ -1,50 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 import { SlOptionsVertical } from "react-icons/sl";
 import "./ListaProductos.css"; // Importar estilos CSS
 
 const ListaProductos = () => {
-  // const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
-  // const [busqueda, setBusqueda] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 8;
-  const [openMenus, setOpenMenus] = useState(Array(8).fill(false)); // Estado para controlar la visibilidad de los menús desplegables
+  const [openMenus, setOpenMenus] = useState(Array(8).fill(false));
+  const [products, setProducts] = useState([]); // Estado para almacenar los productos obtenidos de la base de datos
+  const [categories, setCategories] = useState([]); // Estado para almacenar las categorías obtenidas de la base de datos
+  const [selectedCategory, setSelectedCategory] = useState(""); // Estado para almacenar la categoría seleccionada
 
+  // Obtener productos de la base de datos
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("http://localhost:8080/productos");
+        if (!response.ok) {
+          throw new Error("Error al cargar los productos");
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
 
-  // Array de productos hardcodeados
-  const products = [
-    { id: 1, name: "Producto 1", description: "Descripción del Producto 1" },
-    { id: 2, name: "Producto 2", description: "Descripción del Producto 2" },
-    { id: 3, name: "Producto 3", description: "Descripción del Producto 3" },
-    { id: 4, name: "Producto 4", description: "Descripción del Producto 4" },
-    { id: 5, name: "Producto 5", description: "Descripción del Producto 5" },
-    { id: 6, name: "Producto 6", description: "Descripción del Producto 6" },
-    { id: 7, name: "Producto 7", description: "Descripción del Producto 7" },
-    { id: 8, name: "Producto 8", description: "Descripción del Producto 8" },
-    { id: 9, name: "Producto 9", description: "Descripción del Producto 9" },
-    { id: 10, name: "Producto 10", description: "Descripción del Producto 10" },
-    { id: 11, name: "Producto 11", description: "Descripción del Producto 11" },
-    { id: 12, name: "Producto 12", description: "Descripción del Producto 12" },
-    { id: 13, name: "Producto 13", description: "Descripción del Producto 13" },
-    { id: 14, name: "Producto 14", description: "Descripción del Producto 14" },
-    { id: 15, name: "Producto 15", description: "Descripción del Producto 15" },
-    { id: 16, name: "Producto 16", description: "Descripción del Producto 16" },
-    { id: 17, name: "Producto 17", description: "Descripción del Producto 17" },
-    { id: 18, name: "Producto 18", description: "Descripción del Producto 18" },
-    { id: 19, name: "Producto 19", description: "Descripción del Producto 19" },
-    { id: 20, name: "Producto 20", description: "Descripción del Producto 20" },
-  ];
+    fetchProducts();
+  }, []);
 
-  // // Función para manejar cambios en la búsqueda
-  // const handleBusquedaChange = (event) => {
-  //   setBusqueda(event.target.value);
-  // };
+  // Obtener categorías de la base de datos
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await fetch("http://localhost:8080/categorias/listar");
+        if (!response.ok) {
+          throw new Error("Error al cargar las categorías");
+        }
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
 
-  // // Función para manejar cambios en la selección de categoría
-  // const handleCategoriaChange = (event) => {
-  //   setCategoriaSeleccionada(event.target.value);
-  // };
+    fetchCategories();
+  }, []);
 
   // Lógica para obtener los productos paginados
   const indexOfLastProduct = currentPage * productsPerPage;
@@ -69,25 +71,61 @@ const ListaProductos = () => {
     setOpenMenus(updatedMenus);
   };
 
-  
+  const handleDelete = async (productId) => {
+    const isConfirmed = window.confirm(
+      "¿Estás seguro de que deseas eliminar este producto?"
+    );
+    if (isConfirmed) {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/productos/${productId}`,
+          {
+            method: "DELETE",
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Error al eliminar el producto");
+        }
+        // Eliminar el producto del estado local
+        setProducts(products.filter((product) => product.id !== productId));
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
+
+  const handleAssignCategory = async (productId, categoryId) => {
+    const isConfirmed = window.confirm(
+      "¿Estás seguro de que deseas asignar esta categoría al producto?"
+    );
+    if (isConfirmed) {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/productos/${productId}/asignarCategoria/${categoryId}`,
+          {
+            method: "PUT",
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Error al asignar la categoría al producto");
+        }
+        // Actualizar el estado local de productos después de asignar la categoría
+        const updatedProducts = products.map((product) => {
+          if (product.id === productId) {
+            return { ...product, categoryId: categoryId }; // Actualizar la categoría del producto
+          }
+          return product;
+        });
+        setProducts(updatedProducts);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
+
   return (
     <div className="productos">
       <h3>Lista de Productos</h3>
-
-      {/* Barra de búsqueda
-      <input
-        type="text"
-        placeholder="Buscar producto..."
-        value={busqueda}
-        onChange={handleBusquedaChange}
-      />
-
-      {Menú desplegable para seleccionar categoría }
-      <select value={categoriaSeleccionada} onChange={handleCategoriaChange}>
-        <option value="">Todas las categorías</option>
-        {/* Aquí irían las opciones de categorías }
-      </select> */}
-
       {/* Lista de productos */}
       <div className="row">
         {currentProducts.map((product, index) => (
@@ -107,20 +145,42 @@ const ListaProductos = () => {
               </Card.Body>
               <div className="options-menu">
                 {/* Botón para mostrar/ocultar el menú desplegable */}
-              <Button onClick={() => toggleMenu(index)} className="options-menu-button">
-                <SlOptionsVertical />
-              </Button>
-              {/* Menú desplegable */}
-              {openMenus[index] && (
-                <div className="desplegable-menu">
-                  {/* Contenido del menú desplegable */}
-                  <ul>
-                    <li>Editar</li>
-                    <li>Eliminar</li>
-                    <li>Asignar Categoría</li>
-                  </ul>
-                </div>
-              )}
+                <Button
+                  onClick={() => toggleMenu(index)}
+                  className="options-menu-button"
+                >
+                  <SlOptionsVertical />
+                </Button>
+                {/* Menú desplegable */}
+                {openMenus[index] && (
+                  <div className="desplegable-menu">
+                    {/* Contenido del menú desplegable */}
+                    <ul>
+                      <li>
+                        <Link to={`/admin/editarProducto/${product.id}`}>
+                          Editar
+                        </Link>
+                      </li>
+                      <li onClick={() => handleDelete(product.id)}>Eliminar</li>
+                      <li>
+                        Asignar Categoría
+                        {/* Submenú de categorías */}
+                        <ul>
+                          {categories.map((category) => (
+                            <li
+                              key={category.id}
+                              onClick={() =>
+                                handleAssignCategory(product.id, category.id)
+                              }
+                            >
+                              {category.nombreCategoria}
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </div>
             </Card>
           </div>
@@ -131,9 +191,9 @@ const ListaProductos = () => {
       <ul className="pagination">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} className="page-link">
+            <button onClick={() => paginate(number)} className="page-link">
               {number}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
