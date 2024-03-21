3346aa7d9ed884161452c3eb4b03ca98c024556a
ArmestoG <ger.armesto@gmail.com>
2024-03-21 05:06:44 -0300

    panel admin

--- src/components/ListaProductos/ListaProductos.jsx
+++ src/components/ListaProductos/ListaProductos.jsx
@@ -3,16 +3,24 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Dropdown from "react-bootstrap/Dropdown";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { Stack, Image, Row, Col } from 'react-bootstrap';
+import { SlOptionsVertical } from "react-icons/sl";
+import axios from "axios";
 
 function ListaProductos() {
   const [showModal, setShowModal] = useState(false);
   const [products, setProducts] = useState([]);
+  const [selectedProductId, setSelectedProductId] = useState(null);
+  const [categories, setCategories] = useState([]);
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     fetchProducts();
+    fetchCategories();
   }, []);
 
-  // Función para obtener la lista de productos desde la base de datos
   const fetchProducts = async () => {
     try {
       const response = await fetch("http://localhost:8080/productos/listar");
@@ -26,34 +34,103 @@ function ListaProductos() {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/categorias/listar");
+      if (!response.ok) {
+        throw new Error("Error al cargar las categorías");
+      }
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   const handleCloseModal = () => setShowModal(false);
-  const handleShowModal = () => setShowModal(true);
+  const handleShowModal = (productId, categoryId) => {
+    setSelectedProductId(productId);
+    setCategoriaSeleccionada(categoryId);
+    setShowModal(true);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategoriaSeleccionada(event.target.value);
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:8080/productos/editar`,
+        {
+          id: selectedProductId,
+          categoriaString: categoriaSeleccionada,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+          },
+        }
+      );
+      if (!response.data) {
+        throw new Error("Error al guardar los cambios");
+      }
+      // Actualizar la lista de productos después de modificar la categoría
+      await fetchProducts();
+      handleCloseModal();
+    } catch (error) {
+      console.error("Error:", error);
+      handleCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCategories();
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (windowWidth < 768) {
+    return null;
+  }
 
   return (
     <>
-      <ListGroup>
+      <h3 style={{ textAlign: 'center', margin: '4px' }}>Lista de Productos</h3>
+      <ListGroup style={{ height: `${88 * products.length}px`, overflowY: 'auto', alignItems: 'center' }}>
         {products.map((product) => (
-          <ListGroup key={product.id} as="li" horizontal style={{ width: "100%" }}>
-            <ListGroup.Item style={{ width: "15%" }}>{product.img}</ListGroup.Item>
-            <ListGroup.Item style={{ width: "25%" }}>{product.nombreProducto}</ListGroup.Item>
-            <ListGroup.Item style={{ width: "50%" }}>{product.descripcionProducto}</ListGroup.Item>
-            <ListGroup.Item style={{ width: "10%" }}>
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id={`dropdown-${product.id}`}>
-                  ***
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                  <Dropdown.Item>Editar</Dropdown.Item>
-                  <Dropdown.Item>Eliminar</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item onClick={handleShowModal}>
-                    Asignar Categoria
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </ListGroup.Item>
-          </ListGroup>
+          <ListGroup.Item as="li" key={product.id} style={{ width: '80vw', color: '#E6E0E9', margin: "4px", backgroundColor: "#46599C", borderRadius: "10px", border: 'none', boxShadow: '' }}>
+            <Stack direction="horizontal" gap={5} style={{ justifyContent: 'space-between', alignItems: 'center' }}>
+              <div style={{ display: 'flex' }}>
+                <div className="p-2" style={{ width: "25vw", maxWidth: '88px' }}><Image src={product.imagenSalidaDtoList[0].urlImagen} style={{ maxHeight: '88px', maxWidth: '100%' }} /></div>
+                <div className="p-2" style={{ width: "50vw", maxHeight: '88px' }}>
+                  <Col style={{ height: '100%', display: 'flex', flexDirection: 'column'}}>
+                    <Row style={{ color: '#E6E0E9', fontSize: "16px", fontWeight: "500", justifyContent:"left"}}>{product.nombreProducto}</Row>
+                    <Row style={{ color: '#CAC4D0', fontSize: "14px" , justifyContent:"left"}}>{product.descripcionProducto}</Row>
+                  </Col>
+                </div>
+              </div>
+              <div className="p-2">
+                <Dropdown >
+                  <Dropdown.Toggle style={{ backgroundColor: 'transparent', border: 'none', color: '#E6E0E9' }} >
+                    <SlOptionsVertical />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Editar</Dropdown.Item>
+                    <Dropdown.Item>Eliminar</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleShowModal(product.id,product.categoria.id)}>Asignar Categoría</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </Stack>
+          </ListGroup.Item>
         ))}
       </ListGroup>
 
@@ -64,16 +141,20 @@ function ListaProductos() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Asignar Categoria</Modal.Title>
+          <Modal.Title>Asignar Categoría</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Acá va el menu drop para seleccionar la categoria
+          <select value={categoriaSeleccionada} onChange={handleCategoryChange} className="form-control">
+            {categories.map((category) => (
+              <option key={category.id} value={category.nombreCategoria}>{category.nombreCategoria}</option>
+            ))}
+          </select>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>
             Cancelar
           </Button>
-          <Button variant="primary">Guardar Cambios</Button>
+          <Button variant="primary" onClick={handleSaveChanges}>Guardar Cambios</Button>
         </Modal.Footer>
       </Modal>
     </>
